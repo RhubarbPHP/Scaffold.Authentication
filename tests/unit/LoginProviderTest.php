7536d81afccef77c31e41247772574895daed0cc
Michael Miscampbell <mmiscampbell@gcdtech.com>
2017-11-06 14:27:57 +0000

    Moving to codeception due to RhubarbTestCase having a dependency on it.

--- tests/unit/LoginProviderTest.php
+++ tests/unit/LoginProviderTest.php
@@ -2,12 +2,16 @@
 
 namespace Rhubarb\Scaffolds\Authentication\Tests;
 
+use Rhubarb\Crown\Application;
 use Rhubarb\Crown\Encryption\HashProvider;
 use Rhubarb\Crown\Encryption\Sha512HashProvider;
 use Rhubarb\Crown\Request\Request;
+use Rhubarb\Crown\Request\WebRequest;
 use Rhubarb\Crown\Tests\Fixtures\TestCases\RhubarbTestCase;
+use Rhubarb\Scaffolds\Authentication\DatabaseSchema;
 use Rhubarb\Scaffolds\Authentication\LoginProviders\LoginProvider;
 use Rhubarb\Scaffolds\Authentication\User;
+use Rhubarb\Stem\Schema\SolutionSchema;
 
 class LoginProviderTest extends RhubarbTestCase
 {
@@ -17,6 +21,9 @@ class LoginProviderTest extends RhubarbTestCase
 
         HashProvider::setProviderClassName(Sha512HashProvider::class);
         \Rhubarb\Crown\LoginProviders\LoginProvider::setProviderClassName(LoginProvider::class);
+        SolutionSchema::registerSchema( "AuthenticationSchema", DatabaseSchema::class);
+
+        Application::current()->setCurrentRequest(new WebRequest());
     }
 
     public function testAutoLogin()
@@ -32,14 +39,18 @@ class LoginProviderTest extends RhubarbTestCase
 
         /** @var \Rhubarb\Crown\Request\WebRequest $request */
         $request = Request::current();
-        $request->cookie('lun', "test");
-        $request->cookie('ltk', "anyoldvalue");
+        $request->cookieData['lun'] = "test";
+        $request->cookieData['ltk'] = "anyoldvalue";
 
         $loginProvider = LoginProvider::singleton();
 
         $this->assertFalse($loginProvider->isLoggedIn());
 
-        $request->cookie('ltk', $token);
+        $request->cookieData['ltk'] = $token;
+
+        Application::current()->container()->clearSingleton(LoginProvider::class);
+
+        $loginProvider = LoginProvider::singleton();
 
         $this->assertTrue($loginProvider->isLoggedIn());
     }
